6413cc4dcb8e4b7c83eb4a77f413fb72e88cf80a
Marto <146236816+Marto-first-of-his-name@users.noreply.github.com>
2026-05-16 13:53:37 +0200

    Door works
    this was a pain. Important is: using .set_deferred("disabled", true) instead of .disabled=true on collision shapes.
    The point is to only change collision at the end of the frame and not in the middle of it while other physics calculations are being done. Took me a while to figure out why sometimes the collision wasn't working even though it was set and this was the issue.

--- Scripts/button.gd
+++ Scripts/button.gd
@@ -2,6 +2,8 @@ extends AnimatableBody2D
 
 var isPressed = 0
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@export var target: Node
+@export var methodName: String
 
 var bodiesPressing: Array[PhysicsBody2D]
 
@@ -16,15 +18,22 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+# activate is a bool: 1 does one action, 0 the other. E.g. 1 opens door, 0 closes it.
+func trigger(activate):
+	if target and target.has_method(methodName):
+		target.call(methodName, activate)
+	else:
+		print(str("no target or target has no method called ", methodName))
+
 func set_pressed():
 	var buttonShouldBePressed = not bodiesPressing.is_empty()
 	if buttonShouldBePressed and not isPressed: #i.e. state change from idle to pressed
 		isPressed = 1
-		buttonInteracted.emit(1)
+		trigger(1)
 		animated_sprite_2d.play("pressed")
 	elif not buttonShouldBePressed and isPressed: #i.e. state change from pressed to idle
 		isPressed = 0
-		buttonInteracted.emit(0)
+		trigger(0)
 		animated_sprite_2d.play("idle")
 
 # something touches the button's top
--- Scripts/door.gd
+++ Scripts/door.gd
@@ -1,13 +1,66 @@
 extends AnimatableBody2D
 
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	animated_sprite_2d.play("closes")
+	animated_sprite_2d.set_frame(31)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	print(collision_shape_2d.disabled)
 	pass
 
-func open_door
+func open_close_door(shouldOpen):
+	if shouldOpen:
+		if animated_sprite_2d.is_playing():
+			play_anim_backward_from_current_frame()
+		else:
+			animated_sprite_2d.play("opens")
+	else:
+		enable_disable_collision(1)
+		print("closed?")
+		print(collision_shape_2d.disabled)
+		if animated_sprite_2d.is_playing():
+			play_anim_backward_from_current_frame()
+		else:
+			animated_sprite_2d.play("closes")
+
+func play_anim_backward_from_current_frame():
+	var current_animation = animated_sprite_2d.animation
+	var current_playing_speed = animated_sprite_2d.get_playing_speed()
+	var current_frame = animated_sprite_2d.get_frame()
+	var current_progress = animated_sprite_2d.get_frame_progress()
+	animated_sprite_2d.play(current_animation, 0.0 - current_playing_speed)
+	animated_sprite_2d.set_frame_and_progress(current_frame, current_progress)
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	var current_animation = animated_sprite_2d.animation
+	var current_frame = animated_sprite_2d.get_frame()
+	print(str(current_animation, current_frame))
+	
+	if current_animation == "opens":
+		if current_frame == 0: #door closed
+			#enable_disable_collision(1)
+			pass
+		else: #door open
+			enable_disable_collision(0)
+			print("remov1")
+		
+	else: #current anim is closes
+		if current_frame == 0: #door open
+			enable_disable_collision(0)
+			print("remov2")
+		else: #door closed
+			#enable_disable_collision(1)
+			pass
+
+func enable_disable_collision(shouldEnable):
+	if shouldEnable:
+		collision_shape_2d.set_deferred("disabled", false)
+	else:
+		collision_shape_2d.set_deferred("disabled", true)
